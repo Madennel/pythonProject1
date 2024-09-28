92288057a1c0b1e8eabe7b902595b1d7c8e9aa61
Madennel <MaxBataev@icloud.com>
2024-09-28 15:16:52 +0400

    Переменные

---
+++ Переменные.py
@@ -0,0 +1,8 @@
+# Записываем данные в переменные
+completed_assignments = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_per_assignment = hours_spent / completed_assignments
+
+# Выводим строку на экран
+print("Курс:", course_name, ", всего задач:", completed_assignments, ", затрачено часов:", hours_spent, ", среднее время выполнения", time_per_assignment, "часа.")
