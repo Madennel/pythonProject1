b6dc63aaf44ac4db477083a13450011fcbef733e
Madennel <MaxBataev@icloud.com>
2024-09-28 15:44:16 +0400

    Организация программ и методы строк

---
+++ Организация программ и методы строк.py
@@ -0,0 +1,14 @@
+# module_1_4.py
+
+# Организуем программу
+my_string = input("Я прохожу обучение")
+
+# Выводим количество символов введенного текста
+print(len(my_string))
+
+# Работаем с методами строк
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" ", ""))
+print(my_string[0])
+print(my_string[-1])
