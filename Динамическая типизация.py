1e8e78bd565fedb9726f38bac8e82bbf3711b334
Madennel <MaxBataev@icloud.com>
2024-09-28 15:14:00 +0400

    Динамическая типизация

---
+++ Динамическая типизация.py
@@ -0,0 +1,10 @@
+# Создаем переменные разных типов данных
+name = "Max"
+age = 22
+is_student = True
+
+# Выводим значения переменных на экран
+print("Name:", name)
+print("Age:", age)
+print("New Age:", age + 1)
+print("Is Student:", is_student)
