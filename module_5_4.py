77baccc49674820a5749964c5fb6bef1e1ccf52d
Madennel <MaxBataev@icloud.com>
2024-10-12 20:47:24 +0400

    module_5_4

---
+++ module_5_4.py
@@ -0,0 +1,30 @@
+class House:
+    houses_history = []
+
+    def __new__(cls, name, number_of_floors):
+        # Добавляем название дома в историю
+        cls.houses_history.append(name)
+        return super(House, cls).__new__(cls)
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __del__(self):
+        print(f"{self.name} снесён, но он останется в истории")
+
+# Пример использования класса
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)  # ['ЖК Эльбрус']
+
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)  # ['ЖК Эльбрус', 'ЖК Акация']
+
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)  # ['ЖК Эльбрус', 'ЖК Акация', 'ЖК Матрёшки']
+
+# Удаление объектов
+del h2
+del h3
+
+print(House.houses_history)  # ['ЖК Эльбрус', 'ЖК Акация', 'ЖК Матрёшки']
