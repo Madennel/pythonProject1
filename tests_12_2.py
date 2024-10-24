405497663585c4b326031f0f5a1cc3764c521652
Madennel <MaxBataev@icloud.com>
2024-10-24 21:56:15 +0400

    tests_12_2.py

--- tests_12_2.py
+++ tests_12_2.py
@@ -1,55 +1,53 @@
 import unittest
 
-from runner_and_tournament import Runner, Tournament
+class Tournament:
+    def __init__(self, distance, *participants):
+        self.full_distance = distance
+        self.participants = list(participants)
+
+    def start(self):
+        finishers = {}
+        while self.participants:
+            for participant in self.participants:
+                participant.run()
+                if participant.distance >= self.full_distance:
+                    finishers[len(finishers) + 1] = participant
+                    self.participants.remove(participant)
+        return finishers
+
 
 class TournamentTest(unittest.TestCase):
-    all_results = {}
+    is_frozen = True
 
-    @classmethod
-    def setUpClass(cls):
-        cls.all_results = {}
+    def skip_if_frozen(func):
+        def wrapper(*args, **kwargs):
+            if args[0].is_frozen:
+                print("Тесты в этом кейсе заморожены.")
+                return unittest.skip("Тесты в этом кейсе заморожены.")(func)(*args, **kwargs)
+            return func(*args, **kwargs)
+        return wrapper
 
-    def setUp(self):
-        self.runner1 = Runner("Усэйн", 10)  # Быстрый
-        self.runner2 = Runner("Андрей", 9)  # Средний
-        self.runner3 = Runner("Ник", 3)     # Медленный
-
-    @classmethod
-    def tearDownClass(cls):
-        for key in sorted(cls.all_results.keys()):
-            finishers = cls.all_results[key]
-            result = {place: str(runner) for place, runner in finishers.items()}
-            print(result)
-
-    def test_race_usain_nick(self):
-        tournament = Tournament(90, self.runner1, self.runner3)
+    @skip_if_frozen
+    def test_first_tournament(self):
+        runner1 = Runner("Усэйн", 10)
+        runner2 = Runner("Ник", 3)
+        tournament = Tournament(90, runner1, runner2)
         results = tournament.start()
-        self.all_results[1] = results
-        self.assertTrue(results[max(results.keys())] == self.runner3)
+        self.assertTrue(results[1] == runner1)
 
-    def test_race_andrey_nick(self):
-        tournament = Tournament(90, self.runner2, self.runner3)
+    @skip_if_frozen
+    def test_second_tournament(self):
+        runner1 = Runner("Андрей", 9)
+        runner2 = Runner("Ник", 3)
+        tournament = Tournament(90, runner1, runner2)
         results = tournament.start()
-        self.all_results[2] = results
-        self.assertTrue(results[max(results.keys())] == self.runner3)
+        self.assertTrue(results[1] == runner1)
 
-    def test_race_usain_andrey_nick(self):
-        tournament = Tournament(90, self.runner1, self.runner2, self.runner3)
+    @skip_if_frozen
+    def test_third_tournament(self):
+        runner1 = Runner("Усэйн", 10)
+        runner2 = Runner("Андрей", 9)
+        runner3 = Runner("Ник", 3)
+        tournament = Tournament(90, runner1, runner2, runner3)
         results = tournament.start()
-        self.all_results[3] = results
-        self.assertTrue(results[max(results.keys())] == self.runner3)
-
-    def test_runner_speed_logic(self):
-        tournament = Tournament(100, self.runner1, self.runner3)
-        results = tournament.start()
-        self.assertEqual(list(results.values())[0], self.runner1)
-        self.assertEqual(list(results.values())[-1], self.runner3)
-
-    def test_runner_speed_logic_andrey_nick(self):
-        tournament = Tournament(100, self.runner2, self.runner3)
-        results = tournament.start()
-        self.assertEqual(list(results.values())[0], self.runner2)
-        self.assertEqual(list(results.values())[-1], self.runner3)
-
-if __name__ == "__main__":
-    unittest.main()
+        self.assertTrue(results[1] == runner1)
